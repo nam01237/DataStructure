6283ddf15e2ce2f3ea86ec0a52db72ea33cc7584
남민우 <nam01237@naver.com>
2018-08-26 21:54:34 +0900

    180826 정렬 3가지

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -10,6 +10,32 @@ namespace Sorting
     {
         static void Main(string[] args)
         {
+            int[] bubble = { 4, 8, 1, 11, 5 };
+            Sort.BubbbleSort(bubble);
+
+            foreach (var item in bubble)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+
+            int[] selection = { 4, 8, 1, 11, 5 };
+            Sort.SelectionSort(selection);
+
+            foreach (var item in selection)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+
+            int[] insert = { 4, 8, 1, 11, 5 };
+            Sort.InsertSort(insert);
+
+            foreach (var item in insert)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ Sorting/Sort.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class Sort
+    {
+        public static void SwapInt(ref int x, ref int y)
+        {
+            int temp;
+            temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public static void BubbbleSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if( arr[j] > arr[j + 1])
+                    {
+                        SwapInt(ref arr[j], ref arr[j + 1]);
+                    }
+                }
+            }
+        }
+
+        public static void SelectionSort(int[] arr)
+        {
+            int n = arr.Length - 1;
+            int minIndex;
+
+            for (int i = 0; i < n; i++)
+            {
+                minIndex = i;
+
+                for(int j = i + 1; j <= n; j++)
+                {
+                    if (arr[minIndex] > arr[j])
+                        minIndex = j;
+                }
+
+                SwapInt(ref arr[i], ref arr[minIndex]);
+            }
+        }
+
+        public static void InsertSort(int[] arr)
+        {
+            int pivot;
+
+            for(int i = 1; i < arr.Length; i++)
+            {
+                pivot = arr[i];
+                int j;
+
+                for(j = i - 1; j >= 0; j--)
+                {
+                    if (pivot < arr[j])
+                        arr[j + 1] = arr[j];
+                    else
+                        break;
+                }
+
+                arr[j + 1] = pivot;
+            }
+        }
+    }
+}
